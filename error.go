6066e3933e427fb5c19e8f9b2e201259bf8b91bd
John-Alan Simmons <simmons.johnalan@gmail.com>
2023-08-25 04:04:43 -0400

    Reworked iterators, added error types

--- error.go
+++ error.go
@@ -3,5 +3,8 @@ package corekv
 import "fmt"
 
 var (
-	ErrNotFound = fmt.Errorf("kv: key not found")
+	ErrNotFound     = fmt.Errorf("kv: key not found")
+	ErrEmptyKey     = fmt.Errorf("kv: empty key")
+	ErrValueNil     = fmt.Errorf("kv: value is nil")
+	ErrDiscardedTxn = fmt.Errorf("kv: transaction discarded")
 )
--- kv.go
+++ kv.go
@@ -2,13 +2,47 @@ package corekv
 
 import "context"
 
-type Read interface {
-	Get(ctx context.Context, key []byte) ([]byte, error)
-	Has(ctx context.Context, key []byte) (bool, error)
-	Iterator(ctx context.Context, start, end []byte) Iterator
+// DefaultIterOptions is exactly the default zero value
+// for the IterOptions stuct. It is however recomended
+// to use this if you want the "default" behavior, as
+// these values may change in some way in the future
+// and the old "default" as zero values may produce
+// different behavior.
+var DefaultIterOptions = IterOptions{}
+
+type IterOptions struct {
+	// Prefix iteration, all keys *must* contain
+	// the designated prefix. Prefix takes precendent
+	// over range (start/end). If Prefix is nil, range
+	// will be used (regardless if start/end is nil)
+	Prefix []byte
+
+	// Range iteration from start (inclusive) to end explusive.
+	// Start must be lexigraphically less
+	// then end, unless `nil` is used.
+	//
+	// Start defined as nil will iterate from the first
+	// key in the store.
+	// End defined as nil will iterate till the last key
+	// in the store.
+	Start []byte
+	End   []byte
+
+	// Reverse the direction of the iteration.
+	Reverse bool
+
+	// Only iterate through keys. Calling Value() on the
+	// iterator *must* return nil (no error).
+	KeysOnly bool
 }
 
-type Write interface {
+type Reader interface {
+	Get(ctx context.Context, key []byte) ([]byte, error)
+	Has(ctx context.Context, key []byte) (bool, error)
+	Iterator(ctx context.Context, opts IterOptions) Iterator
+}
+
+type Writer interface {
 	Set(ctx context.Context, key, value []byte) error
 	Delete(ctx context.Context, key []byte) error
 }
@@ -19,30 +53,40 @@ type Iterator interface {
 	Next()
 	Key() []byte
 	Value() ([]byte, error)
+	Seek([]byte)
 	Close(ctx context.Context) error
 }
 
 type Store interface {
-	Read
-	Write
+	Reader
+	Writer
 	Close()
 }
 
 type Batchable interface {
+	Store
 	NewBatch() Batch
 }
 
 type Batch interface {
-	Write
+	Writer
+	Write(context.Context) error
+	Close(context.Context) error
 }
 
 type TxnStore interface {
+	Store
 	NewTxn(readonly bool) Txn
 }
 
 type Txn interface {
-	Read
-	Write
+	Reader
+	Writer
 	Commit(ctx context.Context) error
 	Discard(ctx context.Context) error
+}
+
+type Item interface {
+	Key() []byte
+	Value() []byte
 }
