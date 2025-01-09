e8e03742465258355cd58987f53e775b30b5f551
Andrew Sisley <a.j.sisley@hotmail.com>
2025-01-09 10:23:22 -0500

    Handle badger db closed error
    This is a sneaky bug fix, where for this kind of error, the memory and badger implementations were returning different errors.  This is tested by a test migrated in a later commit (previously badger was untested).

--- error.go
+++ error.go
@@ -7,4 +7,5 @@ var (
 	ErrEmptyKey     = fmt.Errorf("kv: empty key")
 	ErrValueNil     = fmt.Errorf("kv: value is nil")
 	ErrDiscardedTxn = fmt.Errorf("kv: transaction discarded")
+	ErrDBClosed     = fmt.Errorf("kv: datastore closed")
 )
