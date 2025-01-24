c4a8b1065901757edb0305f9f2a62c83e2983f67
Andrew Sisley <a.j.sisley@hotmail.com>
2025-01-24 14:21:55 -0500

    PR FIXUP - Document unwanted badger behaviour

---
+++ test/integration/iterator/reverse_end_seek_valid_value_test.go
@@ -0,0 +1,41 @@
+package iterator
+
+import (
+	"testing"
+
+	"github.com/sourcenetwork/corekv"
+	"github.com/sourcenetwork/corekv/test/action"
+	"github.com/sourcenetwork/corekv/test/integration"
+	"github.com/sourcenetwork/corekv/test/state"
+)
+
+// This test documents unwanted behaviour tracked by issue:
+// https://github.com/sourcenetwork/corekv/issues/38
+func TestIteratorReverseEndSeekValidValue_Badger(t *testing.T) {
+	test := &integration.Test{
+		SupportedStoreTypes: []state.StoreType{
+			state.BadgerStoreType,
+		},
+		Actions: []action.Action{
+			action.Set([]byte("k1"), []byte("v1")),
+			action.Set([]byte("k3"), nil),
+			action.Set([]byte("k4"), []byte("v4")),
+			action.Set([]byte("k2"), []byte("v2")),
+			&action.Iterator{
+				IterOptions: corekv.IterOptions{
+					Reverse: true,
+					End:     []byte("k3"),
+				},
+				ChildActions: []action.IteratorAction{
+					action.Seek([]byte("k4")),
+					action.IsValid(),
+					// `k4` is outside of the upper bound of the iterator and
+					// should not be returned, but it is.
+					action.Value([]byte("v4")),
+				},
+			},
+		},
+	}
+
+	test.Execute(t)
+}
